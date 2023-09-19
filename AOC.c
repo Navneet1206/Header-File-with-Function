c09b347870420a5e802697073973984a1cff4a5f
Navneet Vishwakarma <rajvl132011@gmail.com>
2023-09-19 16:12:20 +0530

    Area of Circle Without

---
+++ AOC.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float i,r,pi=3.14;
+
+    printf("Enter the radius : ");
+    scanf("%f",&r);
+
+    printf("Area Of Circle = %f ",(pi*pow(r,2)));
+
+
+    return 0;
+
+}
